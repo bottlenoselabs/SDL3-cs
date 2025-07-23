70a99eaeb107f5c0206c06092c0839565b468c52
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2025-07-23 01:15:31 -0400

    LazyFoo example 17 mouse events

---
+++ src/cs/examples/Examples.LazyFoo/Examples/E017_MouseEvents/E017_MouseEvents.cs
@@ -0,0 +1,177 @@
+// Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
+
+using bottlenoselabs.SDL;
+using bottlenoselabs.SDL.Input;
+
+#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace
+namespace LazyFoo.Examples;
+
+[UsedImplicitly]
+// ReSharper disable once InconsistentNaming
+public sealed class E017_MouseEvents : ExampleLazyFoo
+{
+    private Texture _textureSpriteSheet = null!;
+
+    private readonly RectangleF[] _spriteSourceRectangles = new RectangleF[4];
+
+    private record struct Sprite
+    {
+        public RectangleF Rectangle;
+        public SpriteSheetIndex SpriteSheetIndex;
+    }
+
+    private readonly Sprite[] _sprites;
+
+    public enum SpriteSheetIndex
+    {
+        MouseOut = 0,
+        MouseMotion = 1,
+        MouseDown = 2,
+        MouseUp = 3
+    }
+
+    public E017_MouseEvents()
+        : base("17 - Mouse Events", isEnabledCreateRenderer2D: true)
+    {
+        var screenWidth = Window.Width;
+        var screenHeight = Window.Height;
+        var screenHalfWidth = screenWidth * 0.5f;
+        var screenHalfHeight = screenHeight * 0.5f;
+
+        _sprites =
+        [
+            new Sprite
+            {
+                Rectangle = new RectangleF { X = 0, Y = 0, Width = screenHalfWidth, Height = screenHalfHeight }
+            },
+            new Sprite
+            {
+                Rectangle = new RectangleF { X = screenHalfWidth, Y = 0, Width = screenHalfWidth, Height = screenHalfHeight }
+            },
+            new Sprite
+            {
+                Rectangle = new RectangleF { X = 0, Y = screenHalfHeight, Width = screenHalfWidth, Height = screenHalfHeight }
+            },
+            new Sprite
+            {
+                Rectangle = new RectangleF { X = screenHalfWidth, Y = screenHalfHeight, Width = screenHalfWidth, Height = screenHalfHeight }
+            }
+        ];
+    }
+
+    public override bool OnStart()
+    {
+        var renderer = Window.Renderer!;
+        renderer.TrySetVSyncMode(VSyncMode.EnabledEveryRefresh);
+
+        return TryLoadAssets();
+    }
+
+    public override void OnExit()
+    {
+    }
+
+    public override void OnMouseMove(in MouseMoveEvent e)
+    {
+        for (var i = 0; i < _sprites.Length; i++)
+        {
+            ref var sprite = ref _sprites[i];
+
+            if (sprite.Rectangle.Contains(e.Position))
+            {
+                sprite.SpriteSheetIndex = SpriteSheetIndex.MouseMotion;
+            }
+            else
+            {
+                sprite.SpriteSheetIndex = SpriteSheetIndex.MouseOut;
+            }
+        }
+    }
+
+    public override void OnMouseDown(in MouseButtonEvent e)
+    {
+        for (var i = 0; i < _sprites.Length; i++)
+        {
+            ref var sprite = ref _sprites[i];
+
+            if (sprite.Rectangle.Contains(e.Position))
+            {
+                sprite.SpriteSheetIndex = SpriteSheetIndex.MouseDown;
+            }
+            else
+            {
+                sprite.SpriteSheetIndex = SpriteSheetIndex.MouseOut;
+            }
+        }
+    }
+
+    public override void OnMouseUp(in MouseButtonEvent e)
+    {
+        for (var i = 0; i < _sprites.Length; i++)
+        {
+            ref var sprite = ref _sprites[i];
+
+            if (sprite.Rectangle.Contains(e.Position))
+            {
+                sprite.SpriteSheetIndex = SpriteSheetIndex.MouseUp;
+            }
+            else
+            {
+                sprite.SpriteSheetIndex = SpriteSheetIndex.MouseOut;
+            }
+        }
+    }
+
+    public override void OnUpdate(TimeSpan deltaTime)
+    {
+    }
+
+    public override void OnDraw(TimeSpan deltaTime)
+    {
+        var renderer = Window.Renderer!;
+
+        renderer.DrawColor = Rgba8U.CornflowerBlue;
+        renderer.Clear();
+
+        for (var i = 0; i < _sprites.Length; i++)
+        {
+            ref var sprite = ref _sprites[i];
+
+            var sourceRectangle = _spriteSourceRectangles[(int)sprite.SpriteSheetIndex];
+            var destinationRectangle = sprite.Rectangle;
+            renderer.RenderTexture(_textureSpriteSheet, sourceRectangle, destinationRectangle);
+        }
+
+        renderer.Present();
+    }
+
+    private bool TryLoadAssets()
+    {
+        var assetsDirectory = Path.Combine(
+            AppContext.BaseDirectory, "Examples", nameof(E017_MouseEvents));
+
+        if (!FileSystem.TryLoadImage(
+                Path.Combine(assetsDirectory, "button.png"), out var imageSurface))
+        {
+            return false;
+        }
+
+        _textureSpriteSheet = Window.Renderer!.CreateTextureFrom(imageSurface!);
+        imageSurface!.Dispose();
+
+        for (var i = 0; i < _spriteSourceRectangles.Length; i++)
+        {
+            _spriteSourceRectangles[i] = new RectangleF
+            {
+                X = 0,
+                Y = i * 200,
+                Width = _textureSpriteSheet.Width,
+                Height = 200
+            };
+        }
+
+        return true;
+    }
+}
