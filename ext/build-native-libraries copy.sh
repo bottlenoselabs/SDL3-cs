5dd60554532240d25ad18fd4f0d0b7d9e4a34fd3
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2025-08-01 19:13:05 -0400

    Build linux-arm64

--- ext/build-native-libraries copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://google.github.io/styleguide/shellguide.html
-# https://github.com/koalaman/shellcheck
-
-# NOTE: It's not ideal, but the SDL base library is built several times.
-
-source ./build-native-library-SDL.sh $1 $2
-source ./build-native-library-SDL_image.sh $1 $2
-source ./build-native-library-SDL_ttf.sh $1 $2
